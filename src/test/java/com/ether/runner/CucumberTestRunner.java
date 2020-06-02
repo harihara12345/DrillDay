6df097ccb33a59a1c7f3d6348586e1b1c56c0ae5
itsupport <hariharavignesh86@gmail.com>
2020-06-02 19:02:49 +0530

    Final Comit

--- src/test/java/com/ether/runner/CucumberTestRunner.java
+++ src/test/java/com/ether/runner/CucumberTestRunner.java
@@ -10,12 +10,13 @@ import org.testng.annotations.BeforeClass;
 import com.cucumber.listener.ExtentCucumberFormatter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+
 @CucumberOptions(
-		plugin = {"json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html","com.cucumber.listener.ExtentCucumberFormatter"},
+        plugin = {"json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html","com.cucumber.listener.ExtentCucumberFormatter"},
         features = {"src/test/java/Feature_Files/"},
         glue = "com.ether.steps",
         dryRun=false,
-        tags = {"@login"}
+        tags = {"@Test"}
         )
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
--- src/test/java/com/ether/steps/SampleTestLogin.java
+++ src/test/java/com/ether/steps/SampleTestLogin.java
@@ -2,18 +2,28 @@ package com.ether.steps;
 
 import java.sql.ResultSet;
 
+import org.openqa.selenium.WebElement;
+
+import com.codoid.products.fillo.Select;
 import com.ether.base.SeleniumDriver;
 import com.ether.base.TestUtil;
 import com.ether.pages.actions.LoginAction;
+import com.ether.pages.locators.HomePageLocators;
 import com.ether.pages.locators.LoginPageLocators;
+import com.ether.pages.locators.SavingPageLocators;
+
 
 //import com.ether.pages.locators.loginPageLocators;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class SampleTestLogin {
 	TestUtil util = new TestUtil();
 	LoginPageLocators locator = new LoginPageLocators();
+	HomePageLocators Homelocator = new HomePageLocators();
+	SavingPageLocators Savinglocator = new SavingPageLocators();
 
 	@Given("^Launch url$")
 	public void loginToUrl() {
@@ -32,10 +42,34 @@ public class SampleTestLogin {
 			userName = result.getString("Username");
 			password = result.getString("Password");
 		}
-		locator.username.sendKeys(userName);
-		locator.password.sendKeys(password);
+		SeleniumDriver.inputValuesInElement(locator.username, userName);
+		SeleniumDriver.inputValuesInElement(locator.username, password);
 		locator.signIn.click();
 		util.closeDBConnection();
+		
 	}
-
+	
+	@When("^Click the Deposit option and Select the Saving option from the drop down$")
+	public void ClickDepositSelectSaving() throws Exception
+	{
+		Homelocator.deposit.click();
+		WebElement s = Savinglocator.Savingoption;
+		Select selectaccount = new Select(s);
+        selectaccount.selectByVisibleText("Savings");
+        
+	}
+	
+	@Then("^Input the amount to be deposited and Click Deposit button$")
+	public void DepositInputAndClick() throws Exception
+	{
+		int Amount=5000;
+        Savinglocator.SavingAmount.sendKeys(Amount);
+        Savinglocator.DepositClick.click();
+	}
+	
+	
+		
+		
+		
+	}
 }
